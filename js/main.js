a04eb6ca527e12abf468534f5674716d1302201a
Bruno Soares <brunosoaresbsv@gmail.com>
2022-12-28 22:07:02 -0300

    Add new patient funcionality created

--- js/main.js
+++ js/main.js
@@ -1,22 +1,52 @@
+function validatePatient(weight, height) {
+    if(weight < 0 || weight > 600 || height < 0 || height > 3){        
+        return false;    
+    }
+    return true;
+}
+
+function addNewPatient(name, weight, height, bf){
+    let tr = document.createElement("tr");
+
+    let tdName = document.createElement("td");
+    tdName.textContent = name;
+
+    let tdWeight = document.createElement("td");
+    tdWeight.textContent = weight;
+
+    let tdHeight = document.createElement("td");
+    tdHeight.textContent = height;
+
+    let tdBF = document.createElement("td");
+    tdBF.textContent = bf;
+
+    let tdBMI = document.createElement("td");
+
+    if(!validatePatient(weight, height)){
+        tdBMI.textContent = "Invalid BMI";
+        tr.classList.add('invalid-patient');        
+    } else {
+        tdBMI.textContent = (weight/(height*height)).toFixed(2);
+    }
+
+    tr.appendChild(tdName);
+    tr.appendChild(tdWeight);
+    tr.appendChild(tdHeight);
+    tr.appendChild(tdBF);
+    tr.appendChild(tdBMI);
+
+    let tablePatients = document.getElementById("patients-table");
+
+    tablePatients.appendChild(tr);
+}
+
 let patients = document.querySelectorAll(".patient");
 
-patients.forEach((patient) => {
-    let validWeight = true;
-    let validHeight = true;
-
+patients.forEach((patient) => {    
     let patientWeight = patient.querySelector(".weight-info").textContent;
-        
-    if(patientWeight < 0 || patientWeight > 600){        
-        validWeight = false;
-    }
-
     let patientHeight = patient.querySelector(".height-info").textContent;
-    
-    if(patientHeight < 0 || patientHeight > 3){
-        validHeight = false;
-    }
 
-    if(validWeight && validHeight) {
+    if(validatePatient(patientWeight, patientHeight)) {
         let patientBMIValue = (patientWeight / (patientHeight*patientHeight)).toFixed(2);
         let patientBMI = patient.querySelector(".bmi-info");
         patientBMI.textContent = patientBMIValue;
@@ -26,3 +56,18 @@ patients.forEach((patient) => {
         patient.classList.add('invalid-patient');   
     }
 });
+
+let buttonAddPatient = document.getElementById("add-patient");
+
+buttonAddPatient.addEventListener("click", (event) => {
+
+    event.preventDefault();
+
+    let nameValue = document.getElementById("name").value;
+    let weightValue = document.getElementById("weight").value;
+    let heightValue = document.getElementById("height").value;
+    let bfValue = document.getElementById("bf").value;
+
+    addNewPatient(nameValue, weightValue, heightValue, bfValue);
+});
+
